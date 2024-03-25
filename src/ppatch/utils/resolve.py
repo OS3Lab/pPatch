6df76a550ae12282aad0d6b5bd797d673f977db6
jingfelix <jingfelix@outlook.com>
2024-03-25 14:32:53 +0800

    chore: split into different file & use absolute reference
    Signed-off-by: jingfelix <jingfelix@outlook.com>

--- src/ppatch/utils/resolve.py
+++ src/ppatch/utils/resolve.py
@@ -1,7 +1,7 @@
 from whatthepatch.patch import Change
 
-from ..model import Hunk, Line
-from .common import find_list_positions
+from ppatch.model import Hunk, Line
+from ppatch.utils.common import find_list_positions
 
 
 def apply_change(
