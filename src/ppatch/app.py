84f725abbadb03cd2323f9a1110de3bde07e73a7
jingfelix <jingfelix@outlook.com>
2024-08-29 20:34:46 +0800

    feat: add clear-cache command
    Signed-off-by: jingfelix <jingfelix@outlook.com>

--- src/ppatch/app.py
+++ src/ppatch/app.py
@@ -30,6 +30,7 @@ def callback(verbose: bool = False, version: bool = False):
 
 from ppatch.commands.apply import apply
 from ppatch.commands.auto import auto
+from ppatch.commands.cache import clear_cache
 from ppatch.commands.get import getpatches
 from ppatch.commands.help import show_settings
 from ppatch.commands.show import show
