207d5b491a7b2afa9627809298f4732cc63dd584
jingfelix <jingfelix@outlook.com>
2024-11-11 17:03:39 +0800

    feat: check current path
    to avoid unexpected `git clean` in other dirs
    
    Signed-off-by: jingfelix <jingfelix@outlook.com>

--- src/ppatch/app.py
+++ src/ppatch/app.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import typer
 from rich.console import Console
@@ -7,6 +8,7 @@ from rich.logging import RichHandler
 logging.basicConfig(level=logging.INFO, handlers=[RichHandler()], format="%(message)s")
 logger = logging.getLogger()
 
+from ppatch.config import settings
 from ppatch.utils.common import post_executed
 
 app = typer.Typer(result_callback=post_executed, no_args_is_help=True)
@@ -27,6 +29,17 @@ def callback(verbose: bool = False, version: bool = False):
         console = Console()
         console.print(f"ppatch version {__version__}")
         raise typer.Exit()
+
+    # Check Current path
+    current_path = os.path.abspath(os.getcwd())
+
+    logger.debug(f"Current path: {current_path}")
+    logger.debug(f"Work directory: {settings.work_dir}")
+
+    if current_path != settings.work_dir:
+        logger.warning(
+            f"Current path ({current_path}) is not the same as the configured work directory ({settings.work_dir})."
+        )
 
 
 from ppatch.commands.apply import apply
